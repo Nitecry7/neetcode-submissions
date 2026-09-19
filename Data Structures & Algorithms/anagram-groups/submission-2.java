b5f85cf63ab765fa9c4c5da6d940ba467892085e
Nitecry7 <59592000+Nitecry7@users.noreply.github.com>
2026-09-19 11:43:18 +0800

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) 
+    {
+        HashMap <String, ArrayList<String>> tempHashMap = new HashMap <>();
+        
+    
+        for(String str : strs)
+        {
+            char [] temp = str.toCharArray();
+            Arrays.sort(temp);
+            String sortedStr = String.valueOf(temp); 
+
+            tempHashMap.putIfAbsent(sortedStr, new ArrayList<String>()); //if the sortedStr key does not exist, then put the key and an arrayList pair into it. If it exists, don't put it.
+            ArrayList<String> group = tempHashMap.get(sortedStr);
+            group.add(str);
+
+        }
+        
+       
+        return new ArrayList<>(tempHashMap.values());
+    }
+}
